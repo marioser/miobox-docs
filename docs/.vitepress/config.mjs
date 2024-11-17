618b2d2843d64212a740e6ab2c4d784acf1fa77f
Mario Serrano <mjserranob@outlook.com>
2024-11-17 17:07:40 -0500

    Uso de archivo public

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     search: {
       provider: 'local'
     },
-    logo: '../assets/Icono.svg',
+    logo: '/Icono.svg',
     // https://vitepress.dev/reference/default-theme-config#navbar
     // https://vitepress.dev/reference/default-theme-config
     nav: [
